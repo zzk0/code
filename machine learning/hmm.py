4db9de15c72a2de7eb7236c4824a540d1d52acf4
zzk0 <zzk9460@gmail.com>
2020-09-17 18:12:36 +0800

    【HMM】前向和后向

--- machine learning/hmm.py
+++ machine learning/hmm.py
@@ -32,6 +32,24 @@ class HMM(object):
         使用前向算法
         """
 
+        # alpha = self._alpha(seq)
+        #
+        # # 汇总
+        # prob = 0.0
+        # for i in range(0, self.N):
+        #     prob += alpha[i][len(seq) - 1]
+
+        beta = self._beta(seq)
+        print(beta)
+
+        # 汇总
+        prob = 0.0
+        for i in range(0, self.N):
+            prob += self.B[i][seq[0]] * self.Pi[i] * beta[i][0]
+
+        return prob
+
+    def _alpha(self, seq):
         # alpha(i)_t, i in [1, N], t in [1, T]
         alpha = np.zeros([self.N, len(seq)])
 
@@ -43,15 +61,26 @@ class HMM(object):
         for t in range(1, len(seq)):
             for i in range(0, self.N):
                 for j in range(0, self.N):
-                    alpha[i][t] += alpha[j][t-1] * self.A[j][i]
+                    alpha[i][t] += alpha[j][t - 1] * self.A[j][i]
                 alpha[i][t] *= self.B[i][seq[t]]
 
-        # 汇总
-        prob = 0.0
-        for i in range(0, self.N):
-            prob += alpha[i][len(seq) - 1]
+        return alpha
 
-        return prob
+    def _beta(self, seq):
+        # beta(i)_t, i in [1, N], t in [1, T]
+        beta = np.zeros([self.N, len(seq)])
+
+        # 初始化, beta(i)_T = 1
+        for i in range(0, self.N):
+            beta[i][len(seq) - 1] = 1
+
+        # 递推
+        for t in range(len(seq) - 2, -1, -1):
+            for i in range(0, self.N):
+                for j in range(0, self.N):
+                    beta[i][t] += self.A[i][j] * self.B[j][seq[t+1]] * beta[j][t+1]
+
+        return beta
 
     def train(self, seqs, states=None):
         """
@@ -89,30 +118,14 @@ class HMM(object):
     def _train_without_tag(self, seqs):
         pass
 
-    def decode(self, seq, algo='viterbi'):
+    def decode(self, seq):
         """
-        预测/解码问题：给定模型，给定观测序列，求解状态序列。有两种方法：近似法、viterbi
-        算法选择有：近似算法, viterbi 算法
+        预测/解码问题：给定模型，给定观测序列，求解状态序列。viterbi 算法
         """
-        if algo == 'viterbi':
-            self._decode_viterbi(seq)
-        else:
-            self._decode_approximate(seq)
-
-    def _decode_viterbi(self, seq):
-        pass
-
-    def _decode_approximate(self, seq):
         pass
 
 
 def main():
-    B = np.array([[2, 3], [1, 1], [3, 4]])
-    row_sum = np.sum(B, axis=1)
-    row_sum = row_sum.reshape([len(row_sum), 1])
-    B = B / row_sum
-    print(B)
-
     A = np.array([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]])
     B = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]])
     Pi = np.array([[0.2], [0.4], [0.4]])
